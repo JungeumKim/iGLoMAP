24a41e8780dbd4d9975da5f92d26c967e84ccc0c
JungeumKim <snukingdom@gmail.com>
2024-01-15 15:47:29 -0600

    m_thresh option) g_dist of only K*m_thresh nearest neighbors.

--- iglo/manifold/glomap_adam.py
+++ iglo/manifold/glomap_adam.py
@@ -141,6 +141,7 @@ class  iGLoMAP():
 
         if self.m_thresh is not None:
             nbrs = NearestNeighbors(n_neighbors=self.n_neighbors *self.m_thresh, metric='precomputed')
+            g_dist[np.isfinite(g_dist)] = 10**10
             nbrs.fit(g_dist)
             g_dist,_ = nbrs.kneighbors(g_dist)
 
